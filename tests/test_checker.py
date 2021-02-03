c5db5eb4aec489edbfeaab758c6c65bfbff90d09
David Wagner <wagdav@gmail.com>
2021-02-03 20:17:08 +0100

    Add TestChecker

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,8 +1,19 @@
 import unittest
 
+import requests_mock
+
 from wmo.checker import Checker
 
 
 class TestChecker(unittest.TestCase):
     def test_it_works(self):
-        self.asserTrue(False)
+        url = "https://example.com"
+
+        checker = Checker()
+
+        with requests_mock.Mocker(session=checker._session) as session_mock:
+            session_mock.get(url, text='session')
+
+            result = checker.check_site(url)
+
+            self.assertEqual(result.status_code, 200)

188ce3bffc50b2913e89efc37148141ce0d7e54d
David Wagner <wagdav@gmail.com>
2021-02-03 21:16:05 +0100

    test

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,19 +1,36 @@
+from unittest.mock import create_autospec
+from datetime import timedelta
 import unittest
 
-import requests_mock
+import requests
 
 from wmo.checker import Checker
 
 
 class TestChecker(unittest.TestCase):
-    def test_it_works(self):
+    def setUp(self):
+        self.session = create_autospec(requests.Session, spec_set=True)
+
+    def test_happy_path(self):
+        url = "https://example.com"
+        http_response = requests.Response()
+        http_response.status_code = 200
+        http_response.elapsed = timedelta(milliseconds=123)
+
+        self.session.get.return_value = http_response
+
+        result = Checker(timeout=1, session=self.session).check_site(url)
+
+        self.session.get.assert_called_once_with(url, timeout=1)
+        self.assertEqual(result.status_code, http_response.status_code)
+        self.assertGreater(result.response_time, 0)
+
+    def test_returns_default_values_when_request_throws(self):
         url = "https://example.com"
 
-        checker = Checker()
+        self.session.get.side_effect = requests.ConnectionError("boom")
 
-        with requests_mock.Mocker(session=checker._session) as session_mock:
-            session_mock.get(url, text='session')
+        result = Checker(timeout=1, session=self.session).check_site(url)
 
-            result = checker.check_site(url)
-
-            self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.status_code, 0)
+        self.assertEqual(result.response_time, 0)
--- wmo/checker.py
+++ wmo/checker.py
@@ -1,6 +1,5 @@
 import logging
-from datetime import timedelta
-from typing import List
+from typing import List, Optional
 from dataclasses import dataclass
 
 import requests
@@ -12,29 +11,35 @@ logger = logging.getLogger(__name__)
 class CheckResult:
     url: str
     status_code: int
-    response_time: timedelta
+    response_time: float
     pattern: str
     matches: List[str]
 
 
 class Checker:
-    def __init__(self, timeout: timedelta):
-        self._session = requests.Session()
-        self.timeout = timeout
+    def __init__(self, timeout: float, session: Optional[requests.Session] = None):
+        self._session = session or requests.Session()
+        self._timeout = timeout
 
     def check_site(self, url) -> CheckResult:
         try:
-            self._session.get(url, timeout=self.timeout)
+            response = self._session.get(url, timeout=self._timeout)
+            return CheckResult(
+                url=url,
+                status_code=response.status_code,
+                response_time=response.elapsed.total_seconds(),
+                pattern="",
+                matches=[],
+            )
         except requests.RequestException:
-            logger.exception("Couldn't fetch the url %s", url)
-
-        return CheckResult(
-            url=url,
-            status_code=200,
-            response_time=timedelta(seconds=1),
-            pattern="",
-            matches=[],
-        )
+            logger.info("Couldn't reach the website %s", url)
+            return CheckResult(
+                url=url,
+                status_code=0,
+                response_time=0,
+                pattern="",
+                matches=[],
+            )
 
     def check_sites(self, urls: List[str]) -> List[CheckResult]:
         return [self.check_site(url) for url in urls]

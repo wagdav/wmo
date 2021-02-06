a5365e8348f4a6cd2da7e6616a56f08816779b64
David Wagner <wagdav@gmail.com>
2021-02-06 11:08:29 +0100

    Write to the database

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1,6 +1,33 @@
+import unittest
+from unittest.mock import MagicMock
+
+from wmo.checker import CheckResult
 from wmo.writer import Writer
 
 
 class TestWriter(unittest.TestCase):
-    def test_first(self):
-        self.assertTrue(False)
+    def test_calls_cursor_execute(self):
+        cursor = MagicMock()
+        some_result = CheckResult(
+            url="https://example.com",
+            status_code=200,
+            response_time=0.123,
+            pattern=None,
+            matches=[],
+        )
+
+        writer = Writer("some_db", "some_username", "some_password")
+        writer.write_cursor(cursor, "some_table", some_result)
+
+        cursor.execute.assert_called_once_with(
+            (
+                "INSERT INTO %s (url, status_code, response_time, pattern, matches) "
+                "VALUES (%s, %s, %s, %s, %s)"
+            ),
+            "some_table",
+            "https://example.com",
+            200,
+            0.123,
+            None,
+            "[]",
+        )
--- wmo/writer.py
+++ wmo/writer.py
@@ -1,4 +1,7 @@
-import psycopg2
+from typing import Any
+import json
+
+import psycopg2  # type: ignore
 
 from .checker import CheckResult
 
@@ -6,5 +9,28 @@ from .checker import CheckResult
 class Writer:
     """ Writes a `CheckResult` into a specified PostgreSQL table """
 
-    def write(self, result: CheckResult) -> None:
-        pass
+    def __init__(self, dbname: str, user: str, password: str):
+        self._dbname = dbname
+        self._user = user
+        self._password = user
+
+    def write(self, table: str, result: CheckResult) -> None:
+        with psycopg2.connect(
+            dbname=self._dbname, user=self._user, password=self._password
+        ) as connection:
+            with connection.cursor() as cursor:
+                self.write_cursor(cursor, table, result)
+
+    def write_cursor(self, cursor: Any, table: str, result: CheckResult) -> None:
+        cursor.execute(
+            (
+                "INSERT INTO %s (url, status_code, response_time, pattern, matches) "
+                "VALUES (%s, %s, %s, %s, %s)"
+            ),
+            table,
+            result.url,
+            result.status_code,
+            result.response_time,
+            result.pattern,
+            json.dumps(result.matches),
+        )
